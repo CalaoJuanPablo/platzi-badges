a29d434b9044108291ed1f24ee19822fe48d196e
Juan Pablo Calao <jcalaop@gmail.com>
2020-06-23 03:05:13 -0500

    migrate Layout to functional component

--- src/templates/Layout/Layout.jsx
+++ src/templates/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Navbar } from "../../components";
 
-export class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        {this.props.children}
-      </div>
-    );
-  }
+export function Layout(props) {
+  return (
+    <div>
+      <Navbar />
+      {props.children}
+    </div>
+  );
 }
